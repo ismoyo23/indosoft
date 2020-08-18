bee3906eacb999b84ac8649ab18949524561f155
M ismoyo setyonowidagdo <ismoyorplb@example.com>
2020-08-18 18:22:54 +0700

    Deploy to Heroku!

---
+++ build/precache-manifest.90adacc8d04d9e62fc50f8938595c0d2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c580f1db63d6276b7d6701fc2d0a6e55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "474aa302bcec5cb7dc98",
+    "url": "/static/css/2.f71eca2f.chunk.css"
+  },
+  {
+    "revision": "a830cf35251e8a29f74e",
+    "url": "/static/css/main.8072d5dd.chunk.css"
+  },
+  {
+    "revision": "474aa302bcec5cb7dc98",
+    "url": "/static/js/2.62f6b184.chunk.js"
+  },
+  {
+    "revision": "5d8326d3efa676d3ffe075800bbd250c",
+    "url": "/static/js/2.62f6b184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a830cf35251e8a29f74e",
+    "url": "/static/js/main.8b631acc.chunk.js"
+  },
+  {
+    "revision": "364cdba291a2d31acaa7",
+    "url": "/static/js/runtime-main.d85d9678.js"
+  },
+  {
+    "revision": "a396a763b1c2d52e88e82a09e88e58b9",
+    "url": "/static/media/booksIcon.a396a763.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d91b710efde5fc42fb645441ca8ceefc",
+    "url": "/static/media/image.d91b710e.jpg"
+  },
+  {
+    "revision": "bcea8f3ed0d91af8188e4874dabd7a1e",
+    "url": "/static/media/img3.bcea8f3e.jpg"
+  },
+  {
+    "revision": "a5c0806140c6f8a819dd7dfa7b68b615",
+    "url": "/static/media/orangBaca3.a5c08061.jpg"
+  },
+  {
+    "revision": "f6787b72b8f836c35a1e0de9a1d59f86",
+    "url": "/static/media/undraw_result_5583.f6787b72.png"
+  }
+]);
